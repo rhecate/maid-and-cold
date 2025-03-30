6b528189e8fe6843cb482234c7ea9e2ea71dbf5d
ikiruse <98178059+ikiruse@users.noreply.github.com>
2025-03-30 09:58:34 -0700

    solved the balcony z order problem

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+@onready var railing = $Railing
+
+func _ready() -> void:
+	railing.z_index = 1
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Maid:
+		railing.z_index = -1
+		#print(railing.z_index)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Maid:
+		railing.z_index = 1
+		#print(railing.z_index)
