195b01d0a80b2a79dd2bab87bebe793a7c5901a9
ikiruse <98178059+ikiruse@users.noreply.github.com>
2025-04-13 13:26:14 -0700

    working on intro scene

--- scripts/intro_scene.gd
+++ scripts/intro_scene.gd
@@ -1,11 +1,15 @@
 extends Node2D
 class_name IntroScene
 
+@export var scene_script : DialogueResource
+
 var _save : SaveGame
-@onready var maid = $Main/Maid
+@onready var maid = $ScenePlayer/Main/Maid
+@onready var animation = $ScenePlayer
 
 func _ready() -> void:
 	_save = SaveGame.load_savegame() as SaveGame
-	_save.saw_intro = true
+	#_save.saw_intro = true
 	maid.global_position = Vector2(-27, 253)
 	maid.set_physics_process(false)
+	animation.play("fillie_walks_in_the_door")
